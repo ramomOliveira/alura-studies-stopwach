752b38397fbf7a5e7333e3f90c66a8543bb4a14f
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-06-27 21:22:50 -0300

    created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import FormComponent from './components/FormComponent';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <FormComponent />
+      <List />
     </div>
   );
 }
